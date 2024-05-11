6c3a0b78727f29cf15d57254edc66487e9f77252
Nikita Dukin <kir.dukin@gmail.com>
2024-05-11 03:33:42 +0200

    feat(*): add some evaluations...

--- src/Data.hs
+++ src/Data.hs
@@ -1,9 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE InstanceSigs #-}
 module Data where
 
 data Op2 = Plus | Min | Mult | Del | Div | Mod | Eql | NotEql | More | MoreEql | Less | LessEql | And | Or deriving(Eq)
 
 instance Show Op2 where
-    -- show :: Op2 -> String
+    show :: Op2 -> String
     show Plus = "+"
     show Min = "-"
     show Mult = "*"
@@ -23,38 +25,42 @@ instance Show Op2 where
 data X = Var String deriving(Eq)
 
 instance Show X where
-    -- show :: X -> String
+    show :: X -> String
     show (Var variable) = variable
 
 
-data E a = AsExpr X | Val a | CE (E a) Op2 (E a) deriving(Eq)
+data E a = VarAsExpr X | Number a | Boolean Bool | Str String | CE (E a) Op2 (E a) deriving(Eq)
+
 
 instance (Num a, Show a) => Show (E a) where
-    -- show ::  E a -> String
-    show (AsExpr var) = show var
-    show (Val a) = show a
+    show :: (Num a, Show a) => E a -> String
+    show (VarAsExpr var) = show var
+    show (Number a) = show a
+    show (Boolean value) = show value
+    show (Str value) = "\"" ++ value ++ "\""
     show (CE expr1 op expr2) = show expr1 ++ " " ++ show op ++ " " ++ show expr2
 
 
 data A a = Arg (E a) deriving(Eq)
 
 instance (Num a, Show a) => Show (A a) where
-    -- show :: (Num a, Show a) => A a -> String
+    show :: (Num a, Show a) => A a -> String
     show (Arg expr) = "Arg: " ++ show expr
 
 
 data F a = Fun String [X] (S a) deriving(Eq)
 
 instance (Num a, Show a) => Show (F a) where
-    -- show :: (Num a, Show a) => F a -> String
-    show (Fun name arguments statement) = "function " ++ name ++ (foldl (\prev curr -> prev ++ " " ++ show curr) "" arguments) ++ " <- " ++ show statement
+    show :: (Num a, Show a) => F a -> String
+    show (Fun name arguments statement) = "function " ++ name ++ foldl (\prev curr -> prev ++ " " ++ show curr) "" arguments ++ " <- " ++ show statement
 
 
 data S a = Pris X (E a) | FunExec (F a) [A a] | Write (E a) | Read X | While (E a) (S a) | If (E a) (S a) (S a) | Seq (S a) (S a) | Skip deriving(Eq)
 
 instance (Num a, Show a) => Show (S a) where
+    show :: (Num a, Show a) => S a -> String
     show (Pris var expr) = show var ++ " = " ++ show expr
-    show (FunExec (Fun name _ _) arguments) = name ++ (foldl (\prev curr -> prev ++ " " ++ show curr) "" arguments)
+    show (FunExec (Fun name _ _) arguments) = name ++ foldl (\prev curr -> prev ++ " " ++ show curr) "" arguments
     show (Write expr) = "write " ++ show expr
     show (Read var) = "read " ++ show var
     show (While cond expr) = "while (" ++ show cond ++ ") do " ++ show expr
@@ -62,7 +68,21 @@ instance (Num a, Show a) => Show (S a) where
     show (Seq statement1 statement2) = show statement1 ++ "; \n" ++ show statement2
 
 -- Example of S:
--- Seq (Pris (Var "x") (Val 8)) ( If (CE (AsExpr (Var "x")) Eql (Val 4)) (Write (AsExpr (Var "Okej"))) (Write (AsExpr (Var "Not okej"))) )
+-- Seq (Pris (Var "x") (Number 8)) ( If (CE (VarAsExpr (Var "x")) Eql (Number 4)) (Write (VarAsExpr (Var "Okej"))) (Write (VarAsExpr (Var "Not okej"))) )
+-- Seq (Pris (Var "x") (Number 8)) (If (CE (VarAsExpr (Var "x")) Eql (Number 4)) (Write (Str "Okej")) (Write (Str "Not okej")))
 -- It means:
 -- x = 8;
 -- if (x == 4) then (write Okej) else (write Not okej)
+
+data FDs a = NullFunction | AddFunction (F a) | ConcatFunctions (FDs a) (FDs a) deriving(Eq)
+
+instance (Num a, Show a) => Show (FDs a) where
+    show NullFunction = ""
+    show (AddFunction function) = show function
+    show (ConcatFunctions function1 function2) = (show function1) ++ "\n\n" ++ (show function2)
+
+
+data P a = Program (FDs a) (S a) deriving(Eq)
+
+instance (Num a, Show a) => Show (P a) where
+    show (Program fd s) = show fd ++ "\n\n" ++ show s
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,100 @@
-module Lib
-    ( someFunc
-    ) where
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data
+import Data.List (lookup)
+import Data.Either (Either)
+
+import Control.Monad (Monad(return), foldM)
+import Control.Monad.State
+import Control.Monad.Trans.State.Lazy (StateT)
+
+import Control.Applicative (Alternative((<|>), empty))
+import Data.Foldable1 (foldlM1, Foldable1 (fold1))
+import qualified Control.Monad.RWS as StateT
+import qualified Control.Monad.RWS as State
+import Data.Maybe (mapMaybe)
+
+someFunc :: S Int
+someFunc = Seq (Pris (Var "x") (Number 8)) (If (CE (VarAsExpr (Var "x")) Eql (Number 4)) (Write (Str "Okej")) (Write (Str "Not okej")))
+
+
+getNumberFromE :: (Num a, Show a) => E a -> Either String a
+getNumberFromE (Number value) = Right value
+getNumberFromE expr = Left $ "Expression " ++ show expr ++ " is not a Number"
+
+getBooleanFromE :: (Num a, Show a) => E a -> Either String Bool
+getBooleanFromE (Boolean value) = Right value
+getBooleanFromE expr = Left $ "Expression " ++ show expr ++ " is not a Boolean"
+
+getStringFromE :: (Num a, Show a) => E a -> Either String String
+getStringFromE (Str value) = Right value
+getStringFromE expr = Left $ "Expression " ++ show expr ++ " is not a String"
+
+-- Функция для вычисления выражения. Возвращает либо Строку, либо Выражение (которое должно содержать либо строку, либо число, либо булеан)
+-- В состоянии хранится кортеж из списка переменных и их значений, а также из списка функций, доступных для использования
+evalExpr :: (Num a, Show a) => E a -> StateT ([(X, a)], [F a]) (Either String) (E a)
+evalExpr (VarAsExpr var) = do
+    (varList, funList) <- get
+
+    case lookup var varList of
+        Just x -> lift $ Right $ Number x
+        Nothing -> lift $ Left $ "Variable " ++ show var ++ " does not exist in context"
+
+evalExpr (Number value) = lift $ return $ Number value
+
+evalExpr (Boolean value) = lift $ return $ Boolean value
+
+evalExpr (Str value) = lift $ return $ Str value
+
+evalExpr (CE expr1 op expr2) = do
+    -- Получаем состояние
+    currentState <- get
+
+    -- Вычисляем левое и правое выражения
+    exprEvaluated1 <- evalExpr expr1
+    exprEvaluated2 <- evalExpr expr2
+
+    -- Определяем возможные комбинации для оператора op
+    -- Например, для операции == мы должны рассмотреть два варианта:
+    -- - сравнение двух чисел
+    -- - сравнение двух Bool
+    -- ...
+    -- В результате получаем массив из кортежей, где содержится:
+    -- 1. Функция для перевода первого Expression в рассматриваемый тип (функции могут быть разные, например с оператором сравнения можно сначала попробовать перевести всё в тип Number, а потом в Boolean)
+    -- 2. Функция для перевода второго Expression в рассматриваемый тип
+    -- 3. Функция для кастинга результата операции
+    -- 4. Первый Expression
+    -- 5. Второй Expression
+    let possibleMappingForOperator = definePossibleMapping op exprEvaluated1 exprEvaluated2
+
+    -- Получаем всевозможные результаты вычислений...
+    let values = map (\(mappingFirst, mappingSecond, cast, expression1, expression2) ->
+            subEval mappingFirst mappingSecond cast expression1 expression2
+            ) possibleMappingForOperator
+
+    -- Избавляемся от StateT эффекта за счёт того, что определяем состояние
+    let a = map (\el -> evalStateT el currentState) values
+
+    -- Получаем финальный результат: Получилось ли осуществить хоть какое-нибудь преобразование...
+    resultFinal <- lift $ foldl (\prev curr -> case prev of
+        Right value -> Right value
+        Left _ -> curr
+        ) (Left "") a
+
+    lift $ return resultFinal
+
+    where
+        definePossibleMapping op exprEvaluated1 exprEvaluated2
+            | op == Min || op == Del || op == Div || Mod == Mod = [(getNumberFromE, getNumberFromE, Number, exprEvaluated1, exprEvaluated2)]
+            | otherwise = [(getNumberFromE, getNumberFromE, Number, exprEvaluated1, exprEvaluated2)]
+
+
+
+subEval :: (Num a, Show a) => (E a -> Either String a) -> (E a -> Either String a) -> (a -> E a) -> E a -> E a -> StateT ([(X, a)], [F a]) (Either String) (E a)
+subEval mappingFirst mappingSecond cast exprEvaluated1 exprEvaluated2 = do
+    result1 <- lift $ mappingFirst exprEvaluated1
+    result2 <- lift $ mappingSecond exprEvaluated2
+
+    return $ cast $ result1 + result2
